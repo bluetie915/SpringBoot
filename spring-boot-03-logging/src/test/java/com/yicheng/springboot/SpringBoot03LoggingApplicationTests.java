e9b00b9ddceada22949b2529241407ac32f41ddf
bluetie915 <1402711057@qq.com>
2020-03-14 21:10:47 +0800

    学习日志

---
+++ spring-boot-03-logging/src/test/java/com/yicheng/springboot/SpringBoot03LoggingApplicationTests.java
@@ -0,0 +1,15 @@
+package com.yicheng.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot03LoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+        
+
+    }
+
+}
